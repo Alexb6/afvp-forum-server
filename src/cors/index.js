94eab88db751d3126e956deb3e7c566f0c4c3545
Alex Lap Tran <info@alex-tran.com>
2021-11-22 00:01:49 +0100

    Add production origins in the cors options

--- src/cors/index.js
+++ src/cors/index.js
@@ -1,7 +1,13 @@
 const cors = require('cors');
 
 const corsOptions = {
-   origin: ['http://localhost:3000', 'http://www.afvp.net'], // '*' or use an [] to specify multiple & specific origins
+   origin: [
+      'http://localhost:3000',
+      'http://www.afvp.net',
+      'https://www.afvp.net/',
+      'http://www.dev.afvp.net/',
+      'https://www.dev.afvp.net/'
+   ], // '*' or use an [] to specify multiple & specific origins
    credentials: true,
    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
